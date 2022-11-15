de74c0b6408e1223ae57de59c4f13daad0073878
Leandro Lanza <tucholanza@gmail.com>
2022-11-15 20:50:17 -0300

    frist

---
+++ main.js
@@ -0,0 +1,104 @@
+productos = [
+    {
+        id:"1",
+        titulo:"mano en el corazon",
+        imagen:"./img/foto1.jpeg",
+        categoria:{
+            id:"corazones",
+            nombre:"Corazon",
+        },
+        precio:"$5500",
+
+    },
+    {
+        id:"2",
+        titulo:"madera-tallada",
+        imagen:"./img/foto2.jpeg",
+        categoria:{
+            id:"tallados",
+            nombre:"Tallado",
+        },
+        precio:"$4000",
+
+    }, {
+        id:"3",
+        titulo:"Amor por las plantas",
+        imagen:"./img/foto3.jpeg",
+        categoria:{
+            id:"corazones",
+            nombre:"Corazon",
+        },
+        precio:"$2000",
+
+    }, {
+        id:"4",
+        titulo:"corazon nochea de las estrellas",
+        imagen:"./img/foto4.jpeg",
+        categoria:{
+            id:"corazones",
+            nombre:"Corazon",
+        },
+        precio:"$2300",
+
+    }, {
+        id:"5",
+        titulo:"corazon con alas",
+        imagen:"./img/foto5.jpeg",
+        categoria:{
+            id:"corazones",
+            nombre:"Corazon",
+        },
+        precio:"$3000",
+
+    }, {
+        id:"6",
+        titulo:"corazon dentro de una cajita",
+        imagen:"./img/foto6.jpeg",
+        categoria:{
+            id:"corazones",
+            nombre:"Corazon",
+        },
+        precio:"$5400",
+
+    }, {
+        id:"7",
+        titulo:"corazon con alas por el mundo",
+        imagen:"./img/foto7.jpeg",
+        categoria:{
+            id:"",
+            nombre:"Corazon",
+        },
+        precio:"$3200",
+    }
+]
+
+const divProductos = document.getElementById('div-container-productos');
+const btnCategory = document.querySelectorAll('btn-categoria');
+
+
+cargarProductos= ()=> {
+    productos.forEach(producto =>{
+        const div = document.createElement('div');
+        div.classList.add('producto');
+        div.innerHTML =`<div class="producto">
+        <img class="prodcuto-imagen" src="${producto.imagen}">
+        <div>
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">${producto.precio}</p>
+            <button class="btd-add-product" id="${producto.id}">Agregar</button>
+        </div>
+    </div> 
+        `;
+        divProductos.append(div);
+    })
+}
+
+
+cargarProductos();
+
+btnCategory.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        e.currentTarget.classList.add('active');
+
+    })
+ })
